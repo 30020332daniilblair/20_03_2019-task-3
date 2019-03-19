ff8c9a8c99689dc0a29f460dcb28c81659a54035
Daniil Blair <30020332@student.toiohomai.ac.nz>
2019-03-20 11:36:16 +1300

    comment

---
+++ task 3/task 3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //My name is Dan and this my programme
+
+            Console.WriteLine("Task 3. The for loop");
+            (int i = 0; i < 11; i++)
+        }
+    }
+}
